02f3b63ce8b010796f082e432fdb95b1eae4ad09
beellz <belal20795@gmail.com>
2020-04-24 20:25:48 +0530

    added favicon , overflow changed , button position changed

--- app.js
+++ app.js
@@ -29,29 +29,6 @@ function clock() {
 setInterval(clock , 100);
 
 
-// document.querySelector(".butt").addEventListener("click", () => {
-//     document.querySelector(".IframeWish").style.display = "none";
-// })
-// let mydoc = document.querySelector(".shift").addEventListener("click", prepareFrame);
-
-// function prepareFrame(e) {
-//     e.preventDefault();
-//     let autoplay = 1
-//     let play = document.querySelector(".shift")
-//     let shift = play.getAttribute("href");
-//     let player = shift
-//     let ifrm = document.createElement("iframe");
-
-//     ifrm.setAttribute("src", `https://www.youtube.com/embed/${player}?autoplay=${autoplay}&loop=1`)
-//     ifrm.style.width = "640px";
-//     ifrm.style.height = "480px";
-//     document.querySelector(".appended").appendChild(ifrm);
-    
-//     console.log(shift)
-// };
-
-// prepareFrame();
-
 
 let butttype = document.querySelector(".butt").addEventListener("click", function() {
     document.getElementById("iframea").src = "https://www.youtube.com/embed/7NOSDKb0HlU?autoplay=1&loop=1"
